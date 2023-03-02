23951a05f822a2053eb6e3547b514da7dc324e6c
Ye Nay Thway <102862836+yenaythway@users.noreply.github.com>
2023-03-02 10:49:06 +0630

    Update client.py

--- client.py
+++ client.py
@@ -1,32 +1,3 @@
-# import socket
-# import threading
-#
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect(('127.0.0.1', 55555))
-# name = input('Enter your name:')
-# def receive():
-#     while True:
-#         try:
-#             message = client.recv(1024).decode('ascii')
-#             if message == 'NICK':
-#                 client.send(name.encode('ascii'))
-#             else:
-#                 print(message)
-#         except:
-#             print('An error occured')
-#             client.close()
-#             break
-#
-# def write():
-#     while True:
-#         message = f'{name}:{input("")}'
-#         client.send(message.encode('ascii'))
-#
-# receive_thread = threading.Thread(target=receive)
-# receive_thread.start()
-# write_thread = threading.Thread(target=write)
-# write_thread.start()
-#
 
 import socket
 import threading
@@ -55,12 +26,9 @@ def write():
 
         if option == "1":
             clients=input("Enter client names separated by commas (e.g. John, Sarah):")
-            # message = input("Enter your message if u want to quit enter 'b' ")
-            # if message.startswith('b'):
-            #     break
-            # else:
-                message = f"Group Chat /{clients}/{name}:{message}"
-                client.send(message.encode('ascii'))
+            message = input("Enter your message ")
+            message = f"Group Chat /{clients}/{name}:{message}"
+            client.send(message.encode('ascii'))
 
 
         elif option == "2":
@@ -76,5 +44,4 @@ receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 write_thread = threading.Thread(target=write)
 write_thread.start()
-
 
